fb4d3f21b1554662c264fe51ed97735449e83e6d
Phil Mashiane <mashianephil@gmail.com>
2020-03-27 23:45:37 +0200

    Created the store

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,9 +2,21 @@ const {
   ApolloServer
 } = require('apollo-server')
 const typeDefs = require('./schema')
+const {
+  createStore
+} = require('./utils')
+
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
+
+const store = createStore()
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI(),
+  })
 })
 
 server.listen().then(({
